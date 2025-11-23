6c1064519f5b843a1c74cb7895a9f394caf94f3c
Md Nafijur Rahman <233580453+nrahman32@users.noreply.github.com>
2025-11-23 21:39:47 +0600

    Add files via upload

---
+++ Insert Elements Using Quadratic Probing.cpp
@@ -0,0 +1,44 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define ll long long
+#define ld long double
+
+void insert_key(vector<ll> &ht, ll key)
+{
+    ll m = ht.size();
+    ll idx = key % m;
+
+    ll st = idx, i = 0;
+    while (ht[idx] != -1)
+    {
+        i++;
+        idx = (idx + i * i) % m;
+
+        if (idx == st)
+        {
+            cout << "Hash table is full " << key << '\n';
+            return;
+        }
+    }
+    ht[idx] = key;
+}
+
+int32_t main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    ll m = 7;
+    vector<ll> ht(m, -1);
+
+    vector<ll> key = {10, 20, 5, 17, 22};
+
+    for (ll x : key)
+        insert_key(ht, x);
+
+    cout << "Hash Table:\n";
+    for (ll i = 0; i < m; i++)
+        cout << i << " -> " << ht[i] << '\n';
+}
